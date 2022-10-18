f60ebe0f02ae85f70586ff4ccdf9c8769aa4f37b
Benedikt Scheungraber <benediktscheungraber@gmail.com>
2022-10-18 21:32:42 +0200

    refactored js app and added feature to adjust participants while counting

--- app.js
+++ app.js
@@ -12,37 +12,57 @@ const submit = document.querySelector(".submit");
 const form = document.getElementById("form");
 const show_info = document.getElementById("show-info");
 
-inputCost.onkeyup = function () {
+function countCost() {
+  counter = window.setInterval(function () {
+    let totalCosts = (parseInt(cost) * parseInt(participants)) / 3600;
+    count += totalCosts;
+    value.textContent = count.toFixed(2) + "€";
+  }, 1000);
+}
+
+function startCount() {
   document.getElementById("cost-output").innerHTML = inputCost.value;
   cost = inputCost.value;
-};
-
-inputParticipants.onkeyup = function () {
+  if (participants === 0 ){
   document.getElementById("participants-output").innerHTML =
     inputParticipants.value;
   participants = inputParticipants.value;
-};
+  }
+  window.clearInterval(counter);
+  countCost();
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains("start")) {
-      window.clearInterval(counter);
-      counter = window.setInterval(function () {
-        let totalCosts = (parseInt(cost) * parseInt(participants)) / 3600;
-        count += totalCosts;
-        value.textContent = count.toFixed(2) + "€";
-      }, 1000);
-      form.style.display = "none";
-      show_info.style.display = "block";
-    } else if (styles.contains("reset")) {
-      window.clearInterval(counter);
-      count = 0;
-      value.textContent = count + "€";
-      form.style.display = "block";
-      show_info.style.display = "none";
-    } else if (styles.contains("stop")) {
-      window.clearInterval(counter);
-    }
-  });
-});
+  form.style.display = "none";
+  show_info.style.display = "block";
+}
+
+function stopCount() {
+  window.clearInterval(counter);
+}
+
+function resetCount() {
+  window.clearInterval(counter);
+  count = 0;
+  value.textContent = count + "€";
+
+        form.style.display = "block";
+        show_info.style.display = "none";
+}
+
+function raise() {
+  window.clearInterval(counter);
+  parsedparticipants = parseInt(participants)
+  parsedparticipants += 1
+  participants = parsedparticipants
+  document.getElementById("participants-output").innerHTML =
+    participants;
+  countCost();
+}
+
+function reduce() {
+  window.clearInterval(counter);
+  parsedparticipants = parseInt(participants);
+  parsedparticipants -= 1;
+  participants = parsedparticipants;
+  document.getElementById("participants-output").innerHTML = participants;
+  countCost();
+}
