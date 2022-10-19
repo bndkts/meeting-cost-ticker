2edc2c1d220b2a4d658fee25a765827aacb81341
Benedikt Scheungraber <benediktscheungraber@gmail.com>
2022-10-19 21:58:03 +0200

    new styling and multi currency support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let count = 0;
 let counter = 0;
 let inputCost = document.getElementById("cost");
 let inputParticipants = document.getElementById("participants");
+let inputCurrency = document.getElementById("currency");
 let cost = 0;
 let participants = 0;
 
@@ -16,12 +17,12 @@ function countCost() {
   counter = window.setInterval(function () {
     let totalCosts = (parseInt(cost) * parseInt(participants)) / 3600;
     count += totalCosts;
-    value.textContent = count.toFixed(2) + "€";
+    value.textContent = inputCurrency.value + count.toFixed(2);
   }, 1000);
 }
 
 function startCount() {
-  document.getElementById("cost-output").innerHTML = inputCost.value;
+  document.getElementById("cost-output").innerHTML = inputCurrency.value + inputCost.value;
   cost = inputCost.value;
   if (participants === 0 ){
   document.getElementById("participants-output").innerHTML =
@@ -42,7 +43,7 @@ function stopCount() {
 function resetCount() {
   window.clearInterval(counter);
   count = 0;
-  value.textContent = count + "€";
+  value.textContent = count + inputCurrency.value;
 
         form.classList.remove("hide");
         show_info.classList.add("hide");
